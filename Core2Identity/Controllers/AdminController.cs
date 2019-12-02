ed8771397cfe9a6df47958aa769067519a01b66c
mehmetberkaysirin <mehmetberkaysirin@gmail.com>
2019-12-02 18:10:35 +0300

    Update bug

--- Core2Identity/Controllers/AdminController.cs
+++ Core2Identity/Controllers/AdminController.cs
@@ -11,9 +11,13 @@ namespace Core2Identity.Controllers
     public class AdminController : Controller
     {
         private UserManager<ApplicationUSer> userManager;
-        public AdminController(UserManager<ApplicationUSer> _userManager)
+        private IPasswordValidator<ApplicationUSer> passwordValidator;
+        private IPasswordHasher<ApplicationUSer> passwordHasher;
+        public AdminController(UserManager<ApplicationUSer> _userManager, IPasswordValidator<ApplicationUSer> _passwordValidator, IPasswordHasher<ApplicationUSer> _passwordHasher)
         {
             userManager = _userManager;
+            passwordValidator = _passwordValidator;
+            passwordHasher = _passwordHasher;
 
         }
         public IActionResult Index()
@@ -56,7 +60,7 @@ namespace Core2Identity.Controllers
         public async Task<IActionResult> Delete(string Id)
         {
             var user = await userManager.FindByIdAsync(Id);
-            if (user!=null)
+            if (user != null)
             {
                 var result = await userManager.DeleteAsync(user);
                 if (result.Succeeded)
@@ -73,9 +77,58 @@ namespace Core2Identity.Controllers
             }
             else
             {
-                ModelState.AddModelError("","User Not Found");
+                ModelState.AddModelError("", "User Not Found");
             }
             return View("Index", userManager.Users);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string Id, string password, string email)
+        {
+            var user = await userManager.FindByIdAsync(Id);
+            if (user != null)
+            {
+
+                user.Email = email;
+                IdentityResult validPass = null;
+                if (string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(userManager, user, password);
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = passwordHasher.HashPassword(user, password);
+                    }
+                    else
+                    {
+                        foreach (var item in validPass.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                }
+                if (validPass.Succeeded)
+                {
+                    var result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+
+            return View(user);
+        }
+
     }
 }
